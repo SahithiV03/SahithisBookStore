fad0177a6f03af5a94cdafd2cb79002db1a31d7a
SahithiV03 <90633643+SahithiV03@users.noreply.github.com>
2021-12-09 23:17:48 -0500

    Add files via upload

---
+++ wwwroot/js/CoverType.js
@@ -0,0 +1,23 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Category/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return ;
+                }, "width": "40%"
+            }
+        ]
+    });
+}

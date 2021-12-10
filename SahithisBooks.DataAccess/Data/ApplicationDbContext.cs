c57b23765d409dfb3c28f24ba5a5f930e844a3ec
SahithiV03 <90633643+SahithiV03@users.noreply.github.com>
2021-12-09 23:05:56 -0500

    Add files via upload

---
+++ SahithisBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using SahithisBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SahithisBookStore.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> CoverType { get; set; }
+        public DbSet<Category> Products { get; set; }
+        public object CoverTypes { get; internal set; }
+    }
+}
